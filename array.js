174207f75f3a0badfcc3f9390b0908e51d4542a6
peekmomo <1804131510@qq.com>
2026-04-29 23:18:57 +0800

    Add files via upload

--- array.js
+++ array.js
@@ -9,3 +9,41 @@ var maxSubArray = function(nums) {
 
     return max;
 };  
+
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals) {
+    intervals.sort((a, b) => a[0] - b[0]);
+    let result = [];
+    result.push(intervals[0]);
+    for (let i = 1; i < intervals.length; i++) {
+        let current = intervals[i];
+        let last = result[result.length - 1];
+        if (current[0] <= last[1]) {
+            last[1] = Math.max(last[1], current[1]);
+        } else {
+            result.push(current);
+        }
+    }
+
+    return result;
+};
+
+
+
+var rotate = function(nums, k) {
+     let n = nums.length;
+    k = k % n;
+    const reverse = (arr, left, right) => {
+        while (left < right) {
+            [arr[left], arr[right]] = [arr[right], arr[left]];
+            left++;
+            right--;
+        }
+    };
+    reverse(nums, 0, n - 1);
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, n - 1);
+};
