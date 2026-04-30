ee799c6e00ac239e716e29aa138f35de838f9498
peekmomo <1804131510@qq.com>
2026-04-30 22:49:32 +0800

    Add files via upload

---
+++ Matrices.js
@@ -0,0 +1,22 @@
+var setZeroes = function(matrix) {
+    let m = matrix.length;
+    let n = matrix[0].length;
+
+    let rows = new Set();
+    let cols = new Set();
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (matrix[i][j] === 0) {
+                rows.add(i);
+                cols.add(j);
+            }
+        }
+    }
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (rows.has(i) || cols.has(j)) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+};
