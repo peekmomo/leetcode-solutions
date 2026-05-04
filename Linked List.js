035733f20b15ed8771c8f67ef8ade84dab7f51b8
peekmomo <1804131510@qq.com>
2026-05-04 23:26:32 +0800

    Add files via upload

---
+++ Linked List.js
@@ -0,0 +1,16 @@
+//相交链表
+var getIntersectionNode = function(headA, headB) {
+    if (!headA || !headB) return null;
+
+    let pA = headA;
+    let pB = headB;
+
+    while (pA !== pB) {
+        pA = pA ? pA.next : headB;
+        pB = pB ? pB.next : headA;
+    }
+
+    return pA; 
+};
+
+//
