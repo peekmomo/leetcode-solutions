929bb03fe9b5aa7db3c7b5ac7378f666a7458730
peekmomo <1804131510@qq.com>
2026-05-04 23:27:13 +0800

    Add files via upload

--- Linked List.js
+++ Linked List.js
@@ -13,4 +13,17 @@ var getIntersectionNode = function(headA, headB) {
     return pA; 
 };
 
-//
+//反转链表
+var reverseList = function(head) {
+   let prev = null;
+    let curr = head;
+
+    while (curr !== null) {
+        let next = curr.next; 
+        curr.next = prev;     
+        prev = curr;          
+        curr = next;         
+    }
+
+    return prev;  
+};
